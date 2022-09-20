96feea27829e9c811176ec74f53894cd79430b8e
agustingiannoni <giannoniagustin@gmail.com>
2022-09-20 14:47:19 +0200

    Miglioramenti

--- app/src/main/java/com/example/healthyvirtuosotest/arch/movies/domain/model/Movie.kt
+++ app/src/main/java/com/example/healthyvirtuosotest/arch/movies/domain/model/Movie.kt
@@ -1,34 +1,39 @@
 package com.example.healthyvirtuosotest.arch.movies.domain.model
 
+
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Movie(
+    @SerializedName("adult")
     val adult: Boolean = false,
+    @SerializedName("backdrop_path")
     val backdropPath: String = "",
-    val belongsToCollection: String? = null,
-    val budget: Int = 0,
-    val genres: List<Genre>? = null,
-    val homepage: String = "",
-    val id: Int? = null,
-    val imdbId: String = "",
+    @SerializedName("genre_ids")
+    val genreIds: List<Int> = listOf(),
+    @SerializedName("id")
+    val id: Int = 0,
+    @SerializedName("original_language")
     val originalLanguage: String = "",
+    @SerializedName("original_title")
     val originalTitle: String = "",
+    @SerializedName("overview")
     val overview: String = "",
+    @SerializedName("popularity")
     val popularity: Double = 0.0,
-    val posterPath: String? ="https://picsum.photos/200",
-    val productionCompanies: List<ProductionCompany>? = null,
-    val productionCountries: List<ProductionCountry>? = null,
+    @SerializedName("poster_path")
+    val posterPath: String = "",
+    @SerializedName("release_date")
     val releaseDate: String = "",
-    val revenue: Int? = 0,
-    val runtime: Int? = 0,
-    val spokenLanguages: List<SpokenLanguage>? = null,
-    val status: String = "",
-    val tagline: String = "",
+    @SerializedName("title")
     val title: String = "",
+    @SerializedName("video")
     val video: Boolean = false,
+    @SerializedName("vote_average")
     val voteAverage: Double = 0.0,
+    @SerializedName("vote_count")
     val voteCount: Int = 0
 ) : Parcelable {
     companion object {
@@ -40,7 +45,6 @@ data class Movie(
                         title = "Movie $i",
                         originalLanguage = "Italian",
                         originalTitle = "Movie Original Title $i",
-                        homepage = "www.ole.com.ar",
                         overview = "Para las mejores amigas Becky y Hunter, la vida trata de superar tus miedos y empujar tus límites. Sin embargo, después de subir hasta la cima de una torre de comunicaciones abandonada, se encuentran atrapadas y sin forma de bajar. A 600 metros del suelo y totalmente alejadas de la civilización, las chicas pondrán a prueba sus habilidades de escaladoras expertas y lucharán desesperadamente por sobrevivir aunque lo tengan todo en contra. ¿Lo conseguirán?",
                     )
                 )
@@ -48,5 +52,4 @@ data class Movie(
             return movies
         }
     }
-
 }
